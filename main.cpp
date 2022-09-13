0578864c7a230c7b0e65f04758644d0e50e93ea1
22cs-09-221081 <113412770+22cs-09-221081@users.noreply.github.com>
2022-09-13 16:20:40 +0500

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,10 @@
+//  main.cpp
+//  lab1
+//  Created by Komil Rajabov on 13/09/22.
+
+#include <iostream>
+using namespace std;
+int main(int argc, const char * argv[]) {
+    cout << "Hello, World!\n";
+    return 0;
+}
